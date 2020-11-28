2dae44f5c0de6419b4b9a0d4cb3a8ca49d809880
Andrew Evans <AEvans@seamsglobal.com>
2020-11-28 10:28:04 +0000

    Initial commit

---
+++ src/views/Cats.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <img src="http://placekitten.com/200/300" />
+    <img src="http://placekitten.com/200/300" />
+    <img src="http://placekitten.com/200/300" />
+    <img src="http://placekitten.com/200/300" />
+  </div>
+</template>
